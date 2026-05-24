3b6a2246030f9e29fe327e04ed59d3d7b6e638ab
xenxynon <valkyrie.xps@gmail.com>
2026-05-24 12:45:04 +0530

    k

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+cat vendor/oneplus/guacamoleb/radio/modem.img.part_* > vendor/oneplus/guacamoleb/radio/modem.img
+cat vendor/oneplus/sm8150-common/proprietary/product/priv-app/OnePlusCamera/OnePlusCamera.apk.part_* > vendor/oneplus/sm8150-common/proprietary/product/priv-app/OnePlusCamera/OnePlusCamera.apk
+cat vendor/oneplus/sm8150-common/proprietary/product/priv-app/OnePlusGallery/OnePlusGallery.apk.part_* > vendor/oneplus/sm8150-common/proprietary/product/priv-app/OnePlusGallery/OnePlusGallery.apk
